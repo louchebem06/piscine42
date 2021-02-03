956acb1eed7d654797687af6ac6ae88fffd16cfd
Ledda Bryan <louchebem06@gmail.com>
2021-02-03 22:08:50 +0100

    norme c10 ex02

--- c/c10/ex02/ft_tail.c
+++ c/c10/ex02/ft_tail.c
@@ -1,21 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tail.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bledda <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/01 12:49:17 by bledda            #+#    #+#             */
+/*   Updated: 2021/02/01 12:49:19 by bledda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft.h"
+
+void	no_file_found(char *av)
+{
+	ft_putstr("tail: ");
+	ft_putstr(av);
+	ft_putstr(": No such file or directory\n");
+}
+
+void	print_file(char *buf, int octes)
+{
+	int		i;
+	int		j;
+
+	j = -1;
+	i = 0;
+	while (buf[i] != 0)
+		i++;
+	if (!(octes >= i))
+		while (++j != octes)
+			i--;
+	else
+		i = 0;
+	while (buf[i] != 0)
+	{
+		write(1, &buf[i], 1);
+		i++;
+	}
+}
 
 void	ft_print_file(char *av, int octes, int title)
 {
 	int		fd;
 	char	buf[BUF_SIZE + 1];
 	int		i;
-	int		j;
 
-	j = -1;
 	i = 0;
 	fd = open(av, O_RDONLY);
 	if (fd == -1)
 	{
 		close(fd);
-		ft_putstr("tail: ");
-		ft_putstr(av);
-		ft_putstr(": No such file or directory\n");
+		no_file_found(av);
 	}
 	else
 	{
@@ -27,18 +63,7 @@ void	ft_print_file(char *av, int octes, int title)
 		}
 		buf[read(fd, buf, BUF_SIZE)] = '\0';
 		close(fd);
-		while (buf[i] != 0)
-			i++;
-		if (!(octes >= i))
-			while (++j != octes)
-				i--;
-		else
-			i = 0;
-		while (buf[i] != 0)
-		{
-			write(1, &buf[i], 1);
-			i++;
-		}
+		print_file(buf, octes);
 	}
 }
 
@@ -47,7 +72,8 @@ void	msg_error(int i, char **av)
 	if (i == 1)
 	{
 		ft_putstr("tail: option requires an argument -- c\n");
-		ft_putstr("usage: tail [-F | -f | -r] [-q] [-b # | -c # | -n #] [file ...]\n");
+		ft_putstr("usage: tail [-F | -f | -r] [-q] [-b # | ");
+		ft_putstr("-c # | -n #] [file ...]\n");
 	}
 	if (i == 2)
 	{
@@ -64,26 +90,23 @@ int		main(int ac, char **av)
 
 	i = -1;
 	error = 0;
-	if (ac >= 2 && ft_strncmp(av[1], "-c", 2) == 0)
+	if (ac == 2 && ft_strncmp(av[1], "-c", 2) == 0)
+		msg_error(1, av);
+	else if (ac > 2 && ft_strncmp(av[1], "-c", 2) == 0)
 	{
-		if (ac == 2)
-			msg_error(1, av);
+		while (av[2][++i] != 0)
+			if (!(av[2][i] >= '0' && av[2][i] <= '9'))
+				error = 1;
+		if (!(av[2][0] >= '0' && av[2][0] <= '9') || error == 1)
+			msg_error(2, av);
 		else
 		{
-			while (av[2][++i] != 0)
-				if (!(av[2][i] >= '0' && av[2][i] <= '9'))
-					error = 1;
-			if (!(av[2][0] >= '0' && av[2][0] <= '9') || error == 1)
-				msg_error(2, av);
-			else
-			{
-				i = 2;
-				while (++i < ac)
-					if (ac > 4)
-						ft_print_file(av[i], ft_atoi(av[2]), 1);
-					else
-						ft_print_file(av[i], ft_atoi(av[2]), 0);
-			}
+			i = 2;
+			while (++i < ac)
+				if (ac > 4)
+					ft_print_file(av[i], ft_atoi(av[2]), 1);
+				else
+					ft_print_file(av[i], ft_atoi(av[2]), 0);
 		}
 	}
 	return (0);
