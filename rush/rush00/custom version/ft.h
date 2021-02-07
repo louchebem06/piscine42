b531a67ebd42fe7b3aa5500103ee07861da9aa20
Ledda Bryan <louchebem06@gmail.com>
2021-02-07 17:56:16 +0100

    update header

--- rush/rush00/custom version/ft.h
+++ rush/rush00/custom version/ft.h
@@ -6,7 +6,7 @@
 /*   By: bledda <bledda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 06:58:00 by bledda            #+#    #+#             */
-/*   Updated: 2021/02/07 17:29:44 by bledda           ###   ########.fr       */
+/*   Updated: 2021/02/07 17:55:35 by bledda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,5 @@ void	line_x(int x, char *left, char *inter, char *right);
 void	line_y(int x, int y, char *left, char *right);
 void	rush(int x, int y);
 int		ft_atoi(char *str);
-void	ft_putstr(char *str);
-void	line_y(int x, int y, char *left, char *right);
-void	line_x(int x, char *left, char *inter, char *right);
 
 #endif
